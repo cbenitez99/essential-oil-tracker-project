4fa979a5c9348f987a3386383d9c68fbb6420734
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-16 16:43:39 -0500

    setup front end application and connected to backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch("http://localhost:3000")
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      setData(data)
+    })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         Essential Oil Tracker
       </header>
+
+      <button onClick={fetchData}>Grab Data</button>
+      {data.map((data)=>(<h1>{data.name}: ${data.price}</h1>))}
+
     </div>
   );
 }
