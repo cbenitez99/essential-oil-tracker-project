f25563ac28e1b230a9cbda5efa586f94b0e8b36f
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-28 18:12:58 -0500

    making login/signup work

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,20 +5,18 @@ import { AppContext } from '../App';
 function Login() {
     const {user, setUser} = useContext(AppContext);
 
-    // const handleSubmit = () => {
-    //     fetch("http:localhost:3000/users")
-    // };
+    const handleSubmit = () => {
+        fetch("/users")
+    };
 
   return (
         <div>
             <h1>Login</h1>
             <div className="canvas">
-                {/* <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <input onChange={(e) => {setUser(e.target.value)}}/>
                     <button onClick={handleSubmit} class="button purple"><span>Login Button</span></button>
-                </form> */}
-           
-                <button class="button purple"><span>Signup Button</span></button>
+                </form>
             </div>
             {console.log(user)}
         </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Signup() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+      let res = await fetch("/users", {
+          method: "POST",
+          body: JSON.stringify({
+          name: name,
+          password: password
+          }),
+      });
+      let resJson = await res.json();
+      if (res.ok) {
+        setUser(resJson)
+          setName("");
+          setPassword("");
+          setMessage("User created successfully");
+      } else {
+          setMessage("Some error occured");
+      }
+      } catch (err) {
+      console.log(err);
+      }
+  };
+
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
-  )
+      <div>
+          <form onSubmit={handleSubmit}>
+              <input
+              type="text"
+              value={name}
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
+              />
+              <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button type="submit">Signup</button>
+
+              <div>{message ? <p>{message}</p> : null}</div>
+          </form>
+      </div>
+  );
 }
 
 export default Signup;
