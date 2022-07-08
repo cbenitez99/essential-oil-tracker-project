13fb348b9661ab827b27897c653e70bdb52a8bda
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-08 16:12:04 -0500

    successfully showing data on products page for all users

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,19 @@
 import React, {useContext} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../App';
 import "./css/productpage.css";
 
 function Products() {
     const {oils} = useContext(AppContext)
+    let navigate = useNavigate();
 
 
   return (
     <div className='products-page'>
         Essential Oil Products
-        {oils.map((oil)=>(<li>{oil.name}, ${oil.price}</li>))}
+        {oils.map((oil)=>(<li key={oil.id}>{oil.name}, ${oil.price}</li>))}
+        <button onClick={()=>{navigate("/")}}>Back</button>
+
     </div>
   );
 };
