7615d510754b9f616cdd105420bb7e39b9c32f02
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-08 20:42:06 -0500

    added some css, and more to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,13 +4,18 @@ import { AppContext } from '../App';
 import { useNavigate } from 'react-router-dom';
 
 function Profile() {
-  const {user} = useContext(AppContext);
+  const {user, oils} = useContext(AppContext);
 
   let navigate = useNavigate();
+
+  const handleAddOil = () => {
+    
+  };
   
   return (
     <div>
         <h1>Hello {user.username}</h1>
+        <h2>Add Products: {oils.map((oil)=><li key={oil.id}>{oil.name} <button onClick={handleAddOil}>+</button></li>)}</h2>
         <button onClick={()=>{navigate("/")}}>Back</button>
     </div>
     
