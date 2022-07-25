879cb337296ddfd53eb347afa4c65ceb5f8c6c2d
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-07-25 14:58:29 -0500

    add oil to inventory: working

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,11 @@ import { useNavigate } from 'react-router-dom';
 
 function Profile() {
   const {user} = useContext(AppContext);
+  const [oilName, setOilName] = useState("");
+  const [oilPrice, setOilPrice] = useState(0);
+  const [oilAmount, setOilAmount] = useState(0);
+
+
   const [userOil, setUserOil] = useState([]);
   const [errors, setErrors] = useState(" ")
 
@@ -28,22 +33,63 @@ function Profile() {
     })
     .then(resp => {
         if(resp.ok){
-          setTimeout(setErrors("Oil Removed"), 3)
-          console.log(errors)
+          setErrors("Oil Removed")
           // navigate(`/`)
         } else {
-          setErrors("No bueno!")
+          setErrors("Something went wrong.")
           alert(`Oh no, something went wrong!`)
         }
     })
   };
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let params = {
+        name: oilName,
+        price: oilPrice,
+        amount: oilAmount,
+        user_id: user.id
+      }
+    fetch("/user_oils", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+        },
+      body: JSON.stringify(params)
+      })
+      .then(resp => {
+        if(resp.ok){
+          resp.json()
+          .then((json) => {
+            setErrors("Oil Added!")
+            // navigate(`/`)
+          })
+        } else {
+          alert(`Oh no, something went wrong!`)
+          resp.json()
+          .then((json) => {
+            setErrors(json.errors)
+        })
+      }
+    });
+  };
+
   return (
     <div>
         <h1>Hello {user.username}</h1>
         <h2>Your Inventory: {userOil.map((oil)=><li key={oil.id}>{oil.name}<button onClick={()=>deleteOil(oil.id)}>Remove Oil</button></li>)}</h2>
         {errors ? <p style={{color : "black"}}>{errors}</p> : null}
         <button onClick={()=>{navigate("/")}}>Back</button>
+
+        <br/>
+        <h2>Add Oil</h2>
+        <form onSubmit={handleSubmit}>  
+          <input onChange={(e) => setOilName(e.target.value)} placeholder="Name"/><br/>
+          <input onChange={(e) => setOilPrice(e.target.value)} placeholder="Price"/>
+          <br/>
+          <input onChange={(e) => setOilAmount(e.target.value)} min="0" type="number" placeholder="Amount"/>
+          <button onClick={handleSubmit}>Add</button>
+        </form>
     </div>
     
   );
