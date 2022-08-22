118ebb1bb150b108533abc9d1aaed67546cfd3e5
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-08-22 18:54:21 -0500

    adding css to login/signup pages. merge into one?

--- src/components/Login.js
+++ src/components/Login.js
@@ -42,20 +42,41 @@ function Login() {
     };
 
     return (
-        <div>
-        <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input onChange={handleChange} type="text" name="username" value={formData.username}/>
-                <label htmlFor="password">Password:</label>
-                <input onChange={handleChange} type="password" name="password" value={formData.password}/>
-                <button type="submit">Login</button>
-                <br/>
+        // <div className=''>
+        // <h1>Login</h1>
+        //     <form onSubmit={handleSubmit}>
+        //         <label htmlFor="username">Username:</label>
+        //         <input onChange={handleChange} type="text" name="username" value={formData.username}/>
+        //         <label htmlFor="password">Password:</label>
+        //         <input onChange={handleChange} type="password" name="password" value={formData.password}/>
+        //         <button type="submit">Login</button>
+        //         <br/>
+        // <p style={{color: "red"}}> {errors}</p>
+        //         <p>Dont have an account? <a href='/signup'>Sign-Up</a></p>
+        //         <p><a href='/signup'>Forgot Password?</a></p>
+        //     </form>
+        // </div>
+        <div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true"/>
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="username" placeholder="User name" value={formData.username}/>
+					<input type="password" name="password" placeholder="Password" value={formData.password}/>
+					<button>Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form onSubmit={handleSubmit}>
+					<label for="chk" aria-hidden="true">Login</label>
+                    <input onChange={handleChange} type="text" placeholder="User name" name="username" value={formData.username}/>
+					<input onChange={handleChange} type="password" name="password" placeholder="Password" value={formData.password}/>
+					<button>Login</button>
+				</form>
                 <p style={{color: "red"}}> {errors}</p>
-                <p>Dont have an account? <a href='/signup'>Sign-Up</a></p>
-                <p><a href='/signup'>Forgot Password?</a></p>
-            </form>
-        </div>
+			</div>
+	    </div>
     );
 };
 
