fe50d273a41fef3dbde8d2ce733f991886284c1d
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-08-23 13:15:23 -0500

    COMMIT MORE OFTEN: adding Css to Profile page. still want to render one input at a time for edit/PATCH

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,28 @@
 import { useContext } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../App';
 import "./css/productpage.css";
 
 function Products() {
-    const {product} = useContext(AppContext);
-    // let navigate = useNavigate();
-
-  return (
-    <div className='products-page'>
-      Global Essential Oil Products:
-      {product.map((prod)=>(<li style={{color : "mistyrose"}} key={prod.id}>{prod.name}, ${prod.price} <br/> {prod.image}</li>))}
-      {/* <button onClick={()=>{navigate("/home")}}>Back</button> */}
-    </div>
-  );
+    const {user, product} = useContext(AppContext);
+    let navigate = useNavigate();
+  if(user){
+    return (
+      <div className='products-page'>
+        Global Essential Oil Products:
+        {product.map((prod)=>(<li style={{color : "mistyrose"}} key={prod.id}>{prod.name}, ${prod.price} <br/> {prod.image}</li>))}
+        <button onClick={()=>{navigate("/home")}}>Back</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className='products-page'>
+        Global Essential Oil Products:
+        {product.map((prod)=>(<li style={{color : "mistyrose"}} key={prod.id}>{prod.name}, ${prod.price} <br/> {prod.image}</li>))}
+        <button onClick={()=>{navigate("/")}}>Back To Login</button>
+      </div>
+    )
+  }
 };
 
 export default Products;
